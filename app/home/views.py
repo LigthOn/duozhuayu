1a802bbde3be827d2add2f23d0f8f265ddcfb7da
Marx <15298393123@163.com>
2021-08-31 21:55:31 +0800

    duozhuayu

--- app/home/views.py
+++ app/home/views.py
@@ -1,29 +1,71 @@
-from app import home
-from app.home.forms import LoginForm
+import bdb
+
+from . import home
+from app import db
+from app.home.forms import LoginForm,RegisterForm
 from flask import render_template, url_for, redirect, flash, session
 
-from app.models import User
+from app.models import User,Goods
+
+"""注册"""
+@home.route("/register/",methods=["POST", "GET"])
+def register():
+    form = RegisterForm()  #实例化registerForm
+    if form.validate_on_submit():  #提交
+        data = form.data  #获取数据
+        user = User(
+            username = data["username"],
+            phone = data["phone"],
+            password = data["password"]
+        )
+        db.session.add(User)
+        db.session.commit()
+        return redirect(url_for("home.login"))
+    return render_template("home/register.html", form=form)
+
 
 """登录"""
 @home.route("/login/", methods=["GET", "POST"])
 def login():
-    if "user_id" in session:        # 如果已经登录，则直接跳转到首页
-        return redirect(url_for("home.index"))
+    #if "user_id" in session:        # 如果已经登录，则直接跳转到首页
+    #    return redirect(url_for("home.index"))
     form = LoginForm()              # 实例化LoginForm类
     if form.validate_on_submit():   # 如果提交
         data = form.data            # 接收表单数据
         # 判断手机号是否存在
-        userphone = User.query.filter_by(phont=data["phone"]).first()    # 获取用户信息
-        if not userphone:
+        user = User.query.filter_by(phone=data["phone"]).first()    # 获取用户信息
+        if not user:
             flash("该手机号未被注册使用！", "err")           # 输出错误信息
             return render_template("home/login.html", form=form)  # 返回登录页
         # 判断用户名和密码是否匹配
-        if not userphone.check_password(data["password"]):     # 调用check_password()方法，检测用户名密码是否匹配
+        if not user.check_password(data["password"]):     # 调用check_password()方法，检测用户名密码是否匹配
             flash("密码错误！", "err")           # 输出错误信息
             return render_template("home/login.html", form=form)  # 返回登录页
 
-        session["user_id"] = userphone.id                # 将user_id写入session, 后面用户判断用户是否登录
-        session["username"] = userphone.username                # 将user_id写入session, 后面用户判断用户是否登录
+        session["user_id"] = user.id                # 将user_id写入session, 后面用户判断用户是否登录
+        #session["username"] = userphone.username                # 将user_id写入session, 后面用户判断用户是否登录
+        session["userphone"] = user.phone  # 将user_id写入session, 后面用户判断用户是否登录
         return redirect(url_for("home.index")) # 登录成功，跳转到首页
+        #return render_template("home/login.html", form=form)  # 返回登录页
 
     return render_template("home/login.html",form=form) # 渲染登录页面模板
+
+
+"""首页"""
+@home.route("/")
+def index():
+    # 获取2个热门商品
+    # hot_goods = Goods.query.order_by(Goods.views_count.desc()).limit(2).all()
+    # # 获取12个新品
+    # new_goods = Goods.query.filter_by(is_new=1).order_by(
+    #                 Goods.addtime.desc()
+    #                     ).limit(12).all()
+    # # 获取12个打折商品
+    # sale_goods = Goods.query.filter_by(is_sale=1).order_by(
+    #                 Goods.addtime.desc()
+    #                     ).limit(12).all()
+    hot_goods = Goods
+    print(Goods)
+    new_goods = []
+    # return render_template('home/index.html',new_goods=new_goods,sale_goods=sale_goods,hot_goods=hot_goods) # 渲染模板
+    return render_template('home/index.html')
--- app/models.py
+++ app/models.py
@@ -1,16 +1,17 @@
 from app import db
 from datetime import datetime
 
+
+"""用户"""
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)  # 编号
     username = db.Column(db.String(100)) # 用户名
     password = db.Column(db.String(100))  # 密码
-    email = db.Column(db.String(100), unique=True)  # 邮箱
     phone = db.Column(db.String(11), unique=True)  # 手机号
     consumption = db.Column(db.DECIMAL(10, 2), default=0)  # 消费额
     addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 注册时间
-    orders = db.relationship('Orders', backref='user')  # 订单外键关系关联
+   # orders = db.relationship('Orders', backref='user')  # 订单外键关系关联
 
     def __repr__(self):
         return '<User %r>' % self.name
@@ -23,3 +24,28 @@ class User(db.Model):
         """
         from werkzeug.security import check_password_hash
         return check_password_hash(self.password, password)
+
+
+# 商品
+class Goods(db.Model):
+    __tablename__ = "goods"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(255))  # 名称
+    # original_price = db.Column(db.DECIMAL(10,2))  # 原价
+    # current_price  = db.Column(db.DECIMAL(10,2))  # 现价
+    # picture = db.Column(db.String(255))  # 图片
+    # introduction = db.Column(db.Text)  # 商品简介
+    # views_count = db.Column(db.Integer,default=0) # 浏览次数
+    # is_sale  = db.Column(db.Boolean(), default=0) # 是否特价
+    # is_new = db.Column(db.Boolean(), default=0) # 是否新品
+    print(id)
+    print(name)
+    # 设置外键
+    #supercat_id = db.Column(db.Integer, db.ForeignKey('supercat.id'))  # 所属大分类
+    #subcat_id = db.Column(db.Integer, db.ForeignKey('subcat.id'))  # 所属小分类
+    #addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
+    #cart = db.relationship("Cart", backref='goods')  # 订单外键关系关联
+    #orders_detail = db.relationship("OrdersDetail", backref='goods')  # 订单外键关系关联
+
+    def __repr__(self):
+        return "<Goods %r>" % self.name
